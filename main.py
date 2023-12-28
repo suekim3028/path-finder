b28f2ade7ab7e4ae207ee5c32d68d8a4c001af8d
ownvoy <wonjun703@naver.com>
2023-12-28 20:34:20 +0900

    DPT upload

---
+++ main.py
@@ -0,0 +1,10 @@
+from model import dpt
+
+
+def main():
+    output = dpt("original_test.jpg")
+    return output
+
+
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
+import numpy as np
+
+
 def downsize(image, m=4, n=8):
-    img_resize = depth.resize((4,8))
+    img_resize = image.resize((4, 8))
     img_array = np.array(img_resize)
     return img_array
