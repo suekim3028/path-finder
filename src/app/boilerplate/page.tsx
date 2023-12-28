483e463ef76f87f19df25800b7c876cce319fdaa
Suebin Kim <k.sue3028@gmail.com>
2023-12-28 09:46:15 +0900

    feat: add loading component

---
+++ src/app/boilerplate/page.tsx
@@ -0,0 +1,9 @@
+import { useRouter } from "next/router";
+
+const Index = () => {
+  const router = useRouter();
+
+  return <>main</>;
+};
+
+export default Index;
---
+++ src/app/loading.tsx
@@ -0,0 +1,8 @@
+import { CircularProgress } from "@mui/material";
+
+const Loading = () => {
+  // You can add any UI inside Loading, including a Skeleton.
+  return <CircularProgress />;
+};
+
+export default Loading;
