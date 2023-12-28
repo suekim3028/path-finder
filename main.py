51716b44afc917c0c782b3cee9ce4a23cbc099a3
Yewon <ga06033@yonsei.ac.kr>
2023-12-28 14:40:21 +0000

    feat: backend with dpt

--- main.py
+++ main.py
@@ -1,10 +1,40 @@
-from model import dpt
+from typing import Annotated
+
+from fastapi import FastAPI, File, UploadFile
+from dl.model import dpt
+from PIL import Image 
+import io 
+import datetime 
+from starlette.middleware.cors import CORSMiddleware
 
 
-def main():
-    output = dpt("original_test.jpg")
-    return output
+app = FastAPI()
+
+origins = [
+    "*"
+    # "localhost:3000",
+    # "35.202.74.253:80"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.post("/images/")
+async def process_frame(file: bytes = File(...)):
+    # read image from bytes 
+    image = Image.open(io.BytesIO(file))
+    timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    print(image, timestamp)
+    return {"depth": dpt(image), 
+            "timestamp": timestamp}
